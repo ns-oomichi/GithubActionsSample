d03f3473fa4e0e0b889b31c7006411adafcaf0a5
ryo oomichi <oomichi@na-s.jp>
2020-02-07 09:47:25 +0900

    create project

---
+++ GithubActionsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubActionsSample
+//
+//  Created by Ryo Oomichi on 2020/02/07.
+//  Copyright © 2020 Ryo Oomichi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
